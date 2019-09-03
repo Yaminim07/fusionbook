e66021f03f0b9a08c6669345633992752b0f0255
Yaminim07 <yaminim@fusioncharts.com>
2019-09-04 02:09:30 +0530

    Svg rating with variable no. of stars

--- components/html/rating.js
+++ components/html/rating.js
@@ -1,39 +1,103 @@
-export default function createSvg (container, obj) {
-  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-  var svgWidth = obj.width
-  var svgHeight = obj.height
+// import { url } from "inspector";
 
-  if (isNaN(svgHeight) || isNaN(svgWidth) || svgHeight === 0 || svgWidth === 0) { console.log('Width or height value not valid') } else {
-    svg.setAttribute('width', svgWidth)
-    svg.setAttribute('height', svgHeight)
-    container.appendChild(svg)
-    var box
+export default class createSvg {
+  constructor (container, obj) {
+    this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    this.svgWidth = obj.width
+    this.svgHeight = obj.height
+    this.stars = obj.noOfStars
+    this.container = container
+    this.obj = obj
+    this.rating = Math.trunc(obj.rating)
+    this.fraction = (obj.rating - this.rating) * 100
+    // this.createGradient()
+  }
 
-    if (obj.horizontal === 'true') { // Horizontal arrangement
-      if ((svgWidth / 5) > svgHeight) { box = svgHeight } else { box = svgWidth / 5 }
+  checkSvgDimensions () {
+    if (isNaN(this.svgHeight) || isNaN(this.svgWidth) || this.svgHeight === 0 || this.svgWidth === 0 || this.svgWidth === undefined || this.svgHeight === undefined) {
+      console.log('Width or height value not valid')
+    }
+  }
 
-      var startWidth = (box / 2) + (svgWidth - (box * 5)) / 2
-      var startHeight = (svgHeight - box) / 2
+  checkStarProperties () {
+    if (typeof this.obj.color !== 'string') {
+      console.log('fill property invalid')
+    }
+    if (typeof this.obj.border !== 'string') {
+      console.log('Stroke property invalid')
+    }
+    if (isNaN(this.obj.noOfStars)) {
+      console.log('No. of stars invalid')
+    }
+  }
 
-      for (let i = 0; i < 5; i++) {
-        createPath(svg, box, startWidth, startHeight, obj)
+  addSvg () {
+    this.svg.setAttribute('width', this.svgWidth)
+    this.svg.setAttribute('height', this.svgHeight)
+    this.container.appendChild(this.svg)
+    createGradient(this.svg, this.fraction)
+    var box, innerBox
+
+    if (this.obj.orientation === 'LtoR' || this.obj.orientation === 'RtoL') { // Horizontal arrangement
+      if ((this.svgWidth / this.stars) > this.svgHeight) { box = this.svgHeight } else { box = this.svgWidth / this.stars }
+      innerBox = box - (2 * this.obj.padding)
+      var startWidth = (box / 2) + (this.svgWidth - (box * this.stars)) / 2
+      var startHeight = (this.svgHeight - box) / 2 + (this.obj.padding)
+      for (let i = 0; i < this.stars; i++) {
+        createPath(this.svg, innerBox, startWidth, startHeight, this.obj, i, this.rating, this.fraction)
         startWidth += box
       }
     } else { // Vertical arrangement
-      if ((svgHeight / 5) > svgWidth) { box = svgWidth } else { box = svgHeight / 5 }
+      if ((this.svgHeight / this.stars) > this.svgWidth) { box = this.svgWidth } else { box = this.svgHeight / this.stars }
 
-      startHeight = (svgHeight - (box * 5)) / 2
-      startWidth = (box / 2) + (svgWidth - box) / 2
+      startHeight = (this.svgHeight - (box * this.stars)) / 2
+      startWidth = (box / 2) + (this.svgWidth - box) / 2
 
-      for (let i = 0; i < 5; i++) {
-        createPath(svg, box, startWidth, startHeight, obj)
+      for (let i = 0; i < this.stars; i++) {
+        createPath(this.svg, box, startWidth, startHeight, this.obj, i)
         startHeight += box
       }
     }
   }
+
+  update (...args) {
+
+  }
+  // var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  // var this.svgWidth = obj.width
+  // var this.svgHeight = obj.height
+  // var this.stars = obj.noOfthis.Stars
+  // if (isNaN(this.svgHeight) || isNaN(this.svgWidth) || this.svgHeight === 0 || this.svgWidth === 0) { console.log('Width or height value not valid') } else if (isNaN(this.stars)) { console.log('Star count not valid') } else {
+  //   svg.setAttribute('width', this.svgWidth)
+  //   svg.setAttribute('height', this.svgHeight)
+  //   container.appendChild(svg)
+  //   var box
+
+  //   if (obj.horizontal === 'true') { // Horizontal arrangement
+  //     if ((this.svgWidth / this.stars) > this.svgHeight) { box = this.svgHeight } else { box = this.svgWidth / this.stars }
+
+  //     var startWidth = (box / 2) + (this.svgWidth - (box * this.stars)) / 2
+  //     var startHeight = (this.svgHeight - box) / 2
+
+  //     for (let i = 0; i < this.stars; i++) {
+  //       createPath(svg, box, startWidth, startHeight, obj)
+  //       startWidth += box
+  //     }
+  //   } else { // Vertical arrangement
+  //     if ((this.svgHeight / this.stars) > this.svgWidth) { box = this.svgWidth } else { box = this.svgHeight / this.stars }
+
+  //     startHeight = (this.svgHeight - (box * this.stars)) / 2
+  //     startWidth = (box / 2) + (this.svgWidth - box) / 2
+
+  //     for (let i = 0; i < this.stars; i++) {
+  //       createPath(svg, box, startWidth, startHeight, obj)
+  //       startHeight += box
+  //     }
+  //   }
+  // }
 }
 
-function createPath (svg, box, startWidth, startHeight, obj) {
+function createPath (svg, box, startWidth, startHeight, obj, index, rating, fraction) {
   var path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
   var d_attr = 'm' + startWidth + ' ' + startHeight + ' ' +
       'l' + -(box / 4) + ' ' + (box / 3) + ' ' +
@@ -47,8 +111,31 @@ function createPath (svg, box, startWidth, startHeight, obj) {
       'l' + -(box / 4) + ' ' + 0 + 'z'
 
   path.setAttribute('d', d_attr)
-  path.style.stroke = obj.border
-  path.style.fill = obj.color
-  path.style.strokeWidth = obj.borderWidth
+  if (index < obj.rating) { path.style.fill = obj.ratedFill } else { path.style.fill = obj.unratedFill }
+  if (fraction !== 0) {
+    if (rating === index) { path.style.fill = 'url(#grad)' }
+  }
+  path.style.stroke = obj.stroke
+  path.style.strokeWidth = obj.strokeWidth
   svg.appendChild(path)
+}
+
+function createGradient (svg, fraction) {
+  var defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs')
+  var grad = document.createElementNS('http://www.w3.org/2000/svg', 'linearGradient')
+  grad.setAttribute('x1', '0%')
+  grad.setAttribute('x2', '100%')
+  grad.setAttribute('y1', '0%')
+  grad.setAttribute('y2', '0%')
+  grad.setAttribute('id', 'grad')
+  var stop1 = document.createElementNS('http://www.w3.org/2000/svg', 'stop')
+  stop1.setAttribute('offset', (fraction + '%'))
+  stop1.setAttribute('stop-color', 'red')
+  var stop2 = document.createElementNS('http://www.w3.org/2000/svg', 'stop')
+  stop2.setAttribute('offset', (fraction + '%'))
+  stop2.setAttribute('stop-color', 'blue')
+  grad.appendChild(stop1)
+  grad.appendChild(stop2)
+  defs.appendChild(grad)
+  svg.appendChild(defs)
 }
--- stories/rating.js
+++ stories/rating.js
@@ -1,43 +1,298 @@
 import { Story, notes, configs } from '../src/lib/story'
-import rating from '../components/html/rating'
+import Rating from '../components/html/rating'
 
 const ratingStory = new Story('rating').addMetas([configs()])
 
-ratingStory.addChapter(
-  'with defaults',
-  story => {
-    var obj = {
-      color: 'red',
-      border: 'blue',
-      borderWidth: 2,
-      horizontal: 'true',
-      width: 500,
-      height: 100
+var obj = {
+  width: 500,
+  height: 100,
+  fill: 'red',
+  ratedFill: 'red',
+  unratedFill: 'blue',
+  stroke: 'blue',
+  strokeWidth: 0,
+  orientation: 'LtoR',
+  noOfStars: 5,
+  rating: 3.5,
+  padding: 20,
+  justifyContent: 'center'
+}
 
-    }
-    rating(story, obj)
+ratingStory.addChapter(
+  'Default Value',
+  story => {
+    var newRating = new Rating(story, obj)
+    newRating.addSvg()
   },
   [
-    notes('Horizontal arrangement.')
+    notes('Rating with default values')
   ]
 )
 
 ratingStory.addChapter(
-  'vertical',
+  'Width or Height value 0',
   story => {
-    var obj = {
-      color: 'red',
-      border: 'blue',
-      borderWidth: 2,
-      horizontal: 'false',
-      width: 100,
-      height: 600
-
-    }
-    rating(story, obj)
+    obj.width = 0
+    obj.height = 0
+    var newRating = new Rating(story, obj)
+    newRating.checkSvgDimensions()
   },
   [
-    notes('Vertical arrangement.')
+    notes('For width = 0 or height = 0 ,no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'Width or Height value negative',
+  story => {
+    obj.width = '-10px'
+    obj.height = 0
+    var newRating = new Rating(story, obj)
+    newRating.checkSvgDimensions()
+  },
+  [
+    notes('For negative values of width or height, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'Width or Height value not a number',
+  story => {
+    obj.width = 'abc'
+    obj.height = 'bcd'
+    var newRating = new Rating(story, obj)
+    newRating.checkSvgDimensions()
+  },
+  [
+    notes('For width = NaN or height = NaN, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'fill value not valid',
+  story => {
+    obj.fill = 'abc'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For invalid fill value, default [fill: red] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'ratedFill value not valid',
+  story => {
+    obj.ratedFill = 'abc'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For invalid ratedFill value, default [ratedFill: red] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'unratedFill value not valid',
+  story => {
+    obj.unratedFill = 'abc'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For invalid unratedFill value, default [unratedFill: blue] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'Stroke value invalid',
+  story => {
+    obj.stroke = 'abc'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For invalid stroke value, default [stroke: blue] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'Stroke-Width value negative',
+  story => {
+    obj.strokeWidth = -3
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For negative strokeWidth value, default [strokeWidth: 0] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'Stroke-Width value not a number',
+  story => {
+    obj.strokeWidth = 'abc'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For strokeWidth = NaN, default [strokeWidth: 0] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'Stroke-Width value greater than maximum permissible',
+  story => {
+    obj.strokeWidth = 100
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For strokeWidth greater than maximum permissible, default [strokeWidth: 0] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'orientation value not valid',
+  story => {
+    obj.orientation = 'LLR'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For invalid orientation value , default [orientation : LtoR] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'padding value NaN',
+  story => {
+    obj.padding = 'abc'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For padding = NaN, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'padding value negative',
+  story => {
+    obj.padding = -10
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For padding value negative, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'padding value greater than maximum permissible',
+  story => {
+    obj.padding = 500
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For padding greater than maximum permissible, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'justifyContent value not valid',
+  story => {
+    obj.justifyContent = 'abc'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For justifyContent value invalid, default [justifyContent: center] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'noOfStars value NaN',
+  story => {
+    obj.noOfStars = 'abc'
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For noOfStars = NaN, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'noOfStars value negative',
+  story => {
+    obj.noOfStars = -10
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For negative value of noOfStars, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'noOfStars value 0',
+  story => {
+    obj.noOfStars = 0
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For noOfStars = 0, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'noOfStars value greater than maximum permissible',
+  story => {
+    obj.noOfStars = 20
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For noOfStars value greater than maximum permissible, no output')
+  ]
+)
+
+ratingStory.addChapter(
+  'rating value NaN',
+  story => {
+    obj.rating = NaN
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For rating value NaN, default [rating : 5] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'rating value negative',
+  story => {
+    obj.rating = NaN
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For negative rating value , default [rating : 5] is applied')
+  ]
+)
+
+ratingStory.addChapter(
+  'rating value greater than noOfStars',
+  story => {
+    obj.rating = NaN
+    var newRating = new Rating(story, obj)
+    newRating.checkStarProperties()
+  },
+  [
+    notes('For rating value greater than noOfStars, default [rating : 5] is applied')
   ]
 )
 
